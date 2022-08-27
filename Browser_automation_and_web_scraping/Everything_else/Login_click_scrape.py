3ca0e519f6459c48e7425fc2b24297e8f149993d
NikolaGospodjinacki1 <nikola.gospodjinacki@levi9.com>
2022-08-27 15:03:09 +0200

    Added script that combines login and scrape

---
+++ Browser_automation_and_web_scraping/Everything_else/Login_click_scrape.py
@@ -0,0 +1,42 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.keys import Keys
+import time
+
+service = Service(executable_path="/usr/bin/chromedriver")
+
+
+def get_driver():
+    # Set options to make browsing easier
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    options.add_argument("disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("http://automated.pythonanywhere.com/login/")
+    return driver
+
+
+def clean_text(text):
+   """ Extract only the temperature from text"""
+   output = float(text.split(": ")[1])
+   return output
+
+def main():
+    driver = get_driver()
+    driver.find_element(by=By.ID, value="id_username").send_keys("automated")
+    time.sleep(2)
+    driver.find_element(by=By.ID, value="id_password").send_keys("automatedautomated" +
+    Keys.RETURN)
+    time.sleep(2)
+    driver.find_element(by=By.XPATH, value="/html/body/nav/div/a").click()
+    print(driver.current_url)
+    time.sleep(2)
+    element = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/h1[2]").text
+    return clean_text(element)
+print(main())
